e0c4891ef39508104156d281e60b38287d30b0d0
goudete <goudetenrique@gmail.com>
2019-12-16 13:49:44 -0600

    register user functionalituy

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from app import login
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,7 +17,7 @@ class User(UserMixin, db.Model):
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    def check_password_hash(self, password):
+    def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
@@ -27,3 +28,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
