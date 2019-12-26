b67fb69655fb3e3ca66911a40df69314b4bffdbc
goudete <goudetenrique@gmail.com>
2019-12-25 17:21:43 -0700

    nice facelift (bootstrap)

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordForm, ResetPasswordRequestForm
 from app.models import User, Post
 from datetime import datetime
 
@@ -144,3 +144,32 @@ def explore():
     return render_template('index.html', title='Explore',
                             posts=posts.items, next_url=next_url,
                             prev_url=prev_url)
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=user.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('check your email for instructions to reset password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html', title='Reset Password',
+                            form=form)
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password was successfuly reset')
+        return redirect(url_for('index'))
+    return render_template('reset_password.html', form=form)
